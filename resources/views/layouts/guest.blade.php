5290a85af970b9c5ace9e82b3d0b14e3f1310b10
James Booz Manumbale <jamesboozmanumbale@gmail.com>
2024-05-01 18:35:28 +0800

    add system tile and company/school name

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -21,6 +21,10 @@
                     <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
                 </a>
             </div>
+            <div class="text-center mt-2">
+                <h1 class="text-xl font-semibold">Golden Gate Colleges</h1>
+                <p class="text-sm text-gray-600">Student Information System</p>
+            </div>
 
             <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
                 {{ $slot }}
