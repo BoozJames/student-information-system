7c8a6f79b86172086e9690b8b00ad05426d31c77
James Booz Manumbale <jamesboozmanumbale@gmail.com>
2024-05-01 17:57:08 +0800

    add new logo and use it as favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,7 +10,9 @@
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
-
+        <link rel="apple-touch-icon" sizes="180x180" href="/images/golden-gate-logo-no-bg.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="/images/golden-gate-logo-no-bg.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="/images/golden-gate-logo-no-bg.png">
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
