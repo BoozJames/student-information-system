5ef6d4f5bc8427b33fffbef7badbc2886ab9914b
James Booz Manumbale <jamesboozmanumbale@gmail.com>
2024-05-02 11:08:16 +0800

    added resource controller and views

--- app/Http/Controllers/ResourceController.php
+++ app/Http/Controllers/ResourceController.php
@@ -4,15 +4,27 @@ namespace App\Http\Controllers;
 
 use App\Models\Resource;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class ResourceController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request)
     {
-        //
+        $resourceQuery = Resource::query();
+
+        if ($request->filled('search')) {
+            $search = $request->input('search');
+            $resourceQuery->where(function ($query) use ($search) {
+                $query->where('resource_name', 'like', "%$search%")
+                    ->orWhere('resource_type', 'like', "%$search%")
+                    ->orWhere('resource_uploaded_by', 'like', "%$search%");
+            });
+        }
+        $resources = $resourceQuery->paginate();
+        return view('resources.index', compact('resources'));
     }
 
     /**
@@ -20,7 +32,11 @@ class ResourceController extends Controller
      */
     public function create()
     {
-        //
+        if (Auth::user()->user_type === 'Teacher' || Auth::user()->user_type === 'Admin') {
+            return view('resources.create');
+        } else {
+            abort(403, 'Unauthorized action.');
+        }
     }
 
     /**
@@ -28,7 +44,22 @@ class ResourceController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        if (Auth::user()->user_type === 'Teacher' || Auth::user()->user_type === 'Admin') {
+            $request->validate([
+                'resource_name' => 'required',
+                'resource_type' => 'required',
+                'resource_filename' => 'required',
+                'resource_url' => 'required',
+                'resource_uploaded_by' => 'required',
+            ]);
+
+            Resource::create($request->all());
+
+            return redirect()->route('resource.index')
+                ->with('success', 'Resource created successfully.');
+        } else {
+            abort(403, 'Unauthorized action.');
+        }
     }
 
     /**
@@ -36,7 +67,7 @@ class ResourceController extends Controller
      */
     public function show(Resource $resource)
     {
-        //
+        return view('resources.show', compact('resource'));
     }
 
     /**
@@ -44,7 +75,11 @@ class ResourceController extends Controller
      */
     public function edit(Resource $resource)
     {
-        //
+        if (Auth::user()->user_type === 'Teacher' || Auth::user()->user_type === 'Admin') {
+            return view('resources.edit', compact('resource'));
+        } else {
+            abort(403, 'Unauthorized action.');
+        }
     }
 
     /**
@@ -52,7 +87,22 @@ class ResourceController extends Controller
      */
     public function update(Request $request, Resource $resource)
     {
-        //
+        if (Auth::user()->user_type === 'Teacher' || Auth::user()->user_type === 'Admin') {
+            $request->validate([
+                'resource_name' => 'required',
+                'resource_type' => 'required',
+                'resource_filename' => 'required',
+                'resource_url' => 'required',
+                'resource_uploaded_by' => 'required',
+            ]);
+
+            $resource->update($request->all());
+
+            return redirect()->route('resource.index')
+                ->with('success', 'Resource updated successfully');
+        } else {
+            abort(403, 'Unauthorized action.');
+        }
     }
 
     /**
@@ -60,6 +110,13 @@ class ResourceController extends Controller
      */
     public function destroy(Resource $resource)
     {
-        //
+        if (Auth::user()->user_type === 'Teacher' || Auth::user()->user_type === 'Admin') {
+            $resource->delete();
+
+            return redirect()->route('resource.index')
+                ->with('success', 'Resource deleted successfully');
+        } else {
+            abort(403, 'Unauthorized action.');
+        }
     }
 }
