fcbccaebe1033c65ef24d01912ab15f8171aa7f3
James Booz Manumbale <jamesboozmanumbale@gmail.com>
2024-05-02 18:50:03 +0800

    update seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,11 +15,11 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         User::factory(10)->create();
-        Resource::factory(10)->create();
+        Resource::factory(50)->create();
 
         User::factory()->create([
             'name' => 'Test User',
-            'email' => 'test@example.com',
+            'email' => 'test@test.test',
             'user_type' => 'Student'
         ]);
 
