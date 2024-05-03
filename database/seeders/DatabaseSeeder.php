4df34460ab748258483e7244b8551323002f6263
James Booz Manumbale <jamesboozmanumbale@gmail.com>
2024-05-04 00:17:39 +0800

    update number of seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        User::factory(10)->create();
+        User::factory(100)->create();
         Resource::factory(50)->create();
 
         User::factory()->create([
