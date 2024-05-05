1b5e857ba6a8b90b3091a8e8ebcc10db2d3b3894
James Booz Manumbale <jamesboozmanumbale@gmail.com>
2024-05-05 23:20:30 +0800

    working show

--- resources/views/grades/show.blade.php
+++ resources/views/grades/show.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Grades Details') }}
+            {{ __('Grade Details') }}
         </h2>
     </x-slot>
 
@@ -10,9 +10,9 @@
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-yellow-300">
                     <div>
-                        <p class="text-gray-900"><strong>Subject Name:</strong> {{ $subject->subject_name }}</p>
-                        <p class="text-gray-900"><strong>Subject Code:</strong> {{ $subject->subject_code }}</p>
-                        <p class="text-gray-900"><strong>Teacher:</strong> {{ $subject->teacher->name }}</p>
+                        <p class="text-gray-900"><strong>User:</strong> {{ $grade->user->name }}</p>
+                        <p class="text-gray-900"><strong>Subject:</strong> {{ $grade->subject->subject_name }}</p>
+                        <p class="text-gray-900"><strong>Value:</strong> {{ $grade->value }}</p>
                     </div>
                 </div>
             </div>
