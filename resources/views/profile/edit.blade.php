f89bd91b963dfc81fe58ba28a9f3c69099f4f48f
James Booz Manumbale <jamesboozmanumbale@gmail.com>
2024-05-07 00:05:31 +0800

    add x-toast

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -5,6 +5,9 @@
         </h2>
     </x-slot>
 
+    <!-- Include the toast component -->
+    <x-toast />
+
     <div class="py-6">
         <div class="max-w-4xl mx-auto sm:px-6 lg:px-8 space-y-6">
             <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
